25a5c1caeaaab090ae596cfffbbf89f25d4ed6cf
suhailkhanqbox <suhail.khan.ftg@gmail.com>
2020-04-15 14:24:42 +0530

    added router guards

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+//import { auth } from '../auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+//export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: auth) { }
 
   ngOnInit(): void {
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,4 +22,30 @@ export class AuthService {
        })
       
    }
+
+   async login(email: string, password: string) {
+    
+     try {
+     await this.afAuth.auth.signInWithEmailAndPassword(email, password)
+     this.router.navigate(['admin/list']);
+     } catch (e) {
+     alert("Error!" + e.message);
+     }
+     }
+    
+     async logout(){
+       await this.afAuth.auth.signOut();
+       localStorage.removeItem('user');
+       this.router.navigate(['admin/login']);
+       }
+      
+       get isLoggedIn(): boolean {
+         const user = JSON.parse(localStorage.getItem('user'));
+         return user !== null;
+         }
+        
+
+
+
+
 }
